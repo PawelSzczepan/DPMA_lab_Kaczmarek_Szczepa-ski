0e71337146f3bc5cb8798eae905de208ee131a8d
KamilStawiarski <kamil.stawiarski@pg.edu.pl>
2025-03-30 14:40:34 +0200

    lab2 tasks added

---
+++ lab2/a.m
@@ -0,0 +1,7 @@
+function out=a(PhiDeg,ArrayPattern,ArrayTaper=[])
+    if length(ArrayTaper)==0
+        out=exp(j*ArrayPattern*pi*sin(PhiDeg*pi/180));
+        return;
+    end
+    out=ArrayTaper.*exp(j*ArrayPattern*pi*sin(PhiDeg*pi/180));
+end
